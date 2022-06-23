87f14ab219143726cb63a81d34ddb8873053ed15
Mohamed Fahad <ssfahad@gmail.com>
2022-06-23 21:26:56 +0530

    save image response to state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+  console.log(images);
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
@@ -15,7 +17,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
