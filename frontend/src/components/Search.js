360e5c3e22cff170f5263bbb34ffd8c3bbb92ea5
Mohamed Fahad <ssfahad@gmail.com>
2022-06-23 17:19:51 +0530

    state added to component

---
+++ frontend/src/components/Search.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
+const Search = (props) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={props.handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control
+                    type="text" value={props.word} onChange={(e) => props.setWord(e.target.value)}
+                    placeholder="Search for new image..." 
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;
